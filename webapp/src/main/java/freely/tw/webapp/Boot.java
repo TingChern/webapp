a045f09b59260d723938e7e4ce7400ab48fbe5eb
chent <chent@LAPTOP-0EIPCCT1>
2021-02-23 15:40:29 +0800

    build main class & change groupId

---
+++ webapp/src/main/java/freely/tw/webapp/Boot.java
@@ -0,0 +1,13 @@
+package freely.tw.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot.class, args);
+	}
+
+}
